629011d460ae4c32134ff1e82ee79e4e1148ddb0
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-23 21:41:35 +0530

    add github data to learn about express

--- Day_01/index.js
+++ Day_01/index.js
@@ -4,6 +4,41 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const githubData = {
+    "login": "saurabhm02",
+    "id": 138717117,
+    "node_id": "U_kgDOCESnvQ",
+    "avatar_url": "https://avatars.githubusercontent.com/u/138717117?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/saurabhm02",
+    "html_url": "https://github.com/saurabhm02",
+    "followers_url": "https://api.github.com/users/saurabhm02/followers",
+    "following_url": "https://api.github.com/users/saurabhm02/following{/other_user}",
+    "gists_url": "https://api.github.com/users/saurabhm02/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/saurabhm02/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/saurabhm02/subscriptions",
+    "organizations_url": "https://api.github.com/users/saurabhm02/orgs",
+    "repos_url": "https://api.github.com/users/saurabhm02/repos",
+    "events_url": "https://api.github.com/users/saurabhm02/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/saurabhm02/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Saurabh Mahapatra",
+    "company": null,
+    "blog": "",
+    "location": "India",
+    "email": null,
+    "hireable": null,
+    "bio": " MERN Stack Developer | Techie Nerd🧑‍💻 | Gamer🎮🎧",
+    "twitter_username": null,
+    "public_repos": 17,
+    "public_gists": 0,
+    "followers": 16,
+    "following": 7,
+    "created_at": "2023-07-05T19:06:36Z",
+    "updated_at": "2023-11-23T12:31:19Z"
+    }
+
 app.get('/', (req, res) =>{
     res.send('Welcome')
 });
@@ -12,8 +47,9 @@ app.get('/about', (req, res) =>{
     res.send('Saurabh')
 });
 
+
 app.get('/login', (req, res) =>{
-    res.send('<div><label>Hello Saurabh</label> <input type="text" ></div>')
+    res.json(githubData);
 });
 
 app.get('/github', (req, res) =>{
