612478306606deb4d145382c0b233f4e3c47d3cb
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-24 23:58:49 +0530

    add backend url in frontend

--- Day_02/Frontend/vite.config.js
+++ Day_02/Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy: {
-      '/api' : 'https://joke-app-dyap.onrender.com',
+      '/api' : 'https://jokesbb.onrender.com',
     },
   },
   plugins: [react()],
